62061a53eb7418e3de6aa93e00bac7c178b39595
tasuku677 <nagataatintern@gmail.com>
2024-06-19 18:36:08 +0900

    RestaurantChain.php being made

---
+++ Restaurants/RestaurantChain.php
@@ -0,0 +1,18 @@
+<?php
+    namespace Restaurants;
+    use Restaurants\RestaurantLocation;
+    use Companies\Company;
+
+    class RestaurantChain extends Company{
+        private int $chainId;
+        private array $restaurantLocation;
+        private int $numberOfLocations;
+        private string $parentCompany;
+
+        public function __construct(int $chainId, array $restaurantLocation, int $numberOfLocations, string $parentCompany){
+            $this->chainId = $chainId;
+            $this->restaurantLocation = $restaurantLocation;
+            $this->numberOfLocations = $numberOfLocations;
+            $this->parentCompany = $parentCompany;
+        }
+    }
